af5503152000e256e7b99a70bcd26be52261fec8
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-03-04 12:06:27 -0300

    feat: add answers for the exercises 1 to 7 of the basic module

--- exercises/exercises.sql
+++ exercises/exercises.sql
@@ -5,3 +5,24 @@ SELECT * FROM cd.facilities;
 
 -- Exercise 2: Retrieve specific columns from a table
 SELECT name, membercost FROM cd.facilities;
+
+-- Exercise 3: Control which rows are retrieved
+SELECT * FROM cd.facilities WHERE membercost > 0;
+
+-- Exercise 4: Control which rows are retrieved - part 2
+SELECT facid, name, membercost, monthlymaintenance FROM cd.facilities WHERE membercost > 0 and membercost < (monthlymaintenance * 1/50);
+
+-- Exercise 5: Basic string searches
+SELECT * FROM cd.facilities WHERE name LIKE '%Tennis%';
+
+-- Exercise 6: Matching against multiple possible values
+SELECT * FROM cd.facilities WHERE facid IN (1, 5);
+SELECT * FROM cd.facilities WHERE facid = 1 OR facid = 5;
+
+-- Exercise 7: Classify results into buckets
+SELECT name,
+	CASE 
+		WHEN monthlymaintenance > 100 THEN 'expensive'
+		ELSE 'cheap'
+	END AS cost
+FROM cd.facilities
