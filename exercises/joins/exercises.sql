683ab650f3fb13c23eb3e0a61ec3d59b7a547e82
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-03-07 11:14:07 -0300

    feat: add support for all joins and subqueries exercises

--- exercises/joins/exercises.sql
+++ exercises/joins/exercises.sql
@@ -63,3 +63,74 @@ WHERE
 ORDER BY 
 	member, facility;
 
+-- Exercise 6: Produce a list of costly bookings
+SELECT 
+	members.firstname || ' ' || members.surname AS member,
+	facilities.name as facility,
+	CASE
+		WHEN members.memid = 0 THEN facilities.guestcost * bookings.slots
+		ELSE facilities.membercost * bookings.slots
+	END AS cost
+FROM 
+	cd.members AS members
+INNER JOIN
+	cd.bookings as bookings ON bookings.memid = members.memid
+INNER JOIN
+	cd.facilities as facilities ON facilities.facid = bookings.facid
+WHERE 
+	bookings.starttime >= '2012-09-14' AND 
+	bookings.starttime < '2012-09-15' AND 
+	((members.memid = 0 AND facilities.guestcost * bookings.slots > 30) OR (members.memid != 0 AND facilities.membercost * bookings.slots > 30 ))
+ORDER BY cost DESC
+
+-- Exercise 7: Produce a list of all members, along with their recommender, using no joins.
+SELECT DISTINCT
+	m1.firstname || ' ' || m1.surname AS member,
+	(
+		SELECT 
+			m2.firstname || ' ' || m2.surname
+		FROM
+			cd.members AS m2
+		WHERE
+			m2.memid = m1.recommendedby
+	) AS recommender
+FROM 
+	cd.members AS m1
+ORDER BY
+	member;
+
+-- Exercise 8: Produce a list of costly bookings, using a subquery
+SELECT 
+    (SELECT firstname || ' ' || surname FROM cd.members WHERE memid = bookings.memid) AS member,
+    (SELECT name FROM cd.facilities WHERE facid = bookings.facid) AS facility,
+    CASE
+        WHEN (
+            SELECT memid FROM cd.members WHERE memid = bookings.memid
+        ) = 0 THEN (
+            SELECT guestcost FROM cd.facilities WHERE facid = bookings.facid
+        ) * bookings.slots
+        ELSE (
+            SELECT membercost FROM cd.facilities WHERE facid = bookings.facid
+        ) * bookings.slots
+    END AS cost
+FROM 
+    cd.bookings
+WHERE 
+    starttime >= '2012-09-14' 
+    AND starttime < '2012-09-15' 
+    AND (
+        (
+            (SELECT memid FROM cd.members WHERE memid = bookings.memid) = 0 
+            AND (
+                SELECT guestcost FROM cd.facilities WHERE facid = bookings.facid
+            ) * bookings.slots > 30
+        ) 
+        OR (
+            (SELECT memid FROM cd.members WHERE memid = bookings.memid) != 0 
+            AND (
+                SELECT membercost FROM cd.facilities WHERE facid = bookings.facid
+            ) * bookings.slots > 30
+        )
+    )
+ORDER BY 
+    cost DESC;
