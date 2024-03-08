992f80cd65011ffe089678da6a6dcc15c8fa7c36
gabr1elpachec0 <gabrielgpacheco12@gmail.com>
2024-03-08 12:02:23 -0300

    feat: add support for all queries

--- exercises/aggregation/exercises.sql
+++ exercises/aggregation/exercises.sql
@@ -169,3 +169,131 @@ GROUP BY
 	MEMBERS.MEMID
 ORDER BY 
 	MEMID;
+
+-- Exercise 15: Produce a list of member names, with each row containing the total member count
+SELECT 
+	COUNT(*) OVER() AS count,
+	firstname,
+	surname
+FROM 
+	cd.members
+ORDER BY joindate;
+
+-- Exercise 16: Produce a numbered list of members
+SELECT
+	ROW_NUMBER() OVER() AS row_number,
+	firstname,
+	surname
+FROM
+	cd.members
+ORDER BY
+	joindate;
+
+-- Exercise 17: Output the facility id that has the highest number of slots booked, again
+SELECT
+	facilities.facid,
+	SUM(slots) AS total
+FROM
+	cd.facilities facilities
+INNER JOIN 
+	cd.bookings bookings ON bookings.facid = facilities.facid
+GROUP BY 
+	facilities.facid
+HAVING
+	SUM(slots) = (SELECT MAX(total) FROM (SELECT SUM(slots) AS total FROM cd.bookings GROUP BY facid) AS subquery);
+
+-- Exercise 18: Rank members by (rounded) hours used
+SELECT 
+	firstname,
+	surname,
+	((SUM(bookings.slots) + 10) / 20) * 10 AS hours,
+	RANK() OVER (ORDER BY ((SUM(bookings.slots) + 10) / 20) * 10 DESC) AS rank
+FROM
+	cd.bookings bookings
+INNER JOIN cd.members members
+	ON bookings.memid = members.memid
+GROUP BY 
+	members.memid
+ORDER BY 
+	rank, 
+	surname, 
+	firstname;
+
+-- Exercise 19: Find the top three revenue generating facilities
+SELECT 
+	name,
+	RANK() OVER (ORDER BY total DESC) AS rank
+FROM 
+	(
+		SELECT
+			facilities.name,
+			SUM(slots * CASE
+					WHEN memid = 0 THEN facilities.guestcost
+					ELSE facilities.membercost
+				END) AS total
+		FROM
+			cd.bookings bookings
+		INNER JOIN 
+			cd.facilities facilities
+			ON bookings.facid = facilities.facid
+		GROUP BY 
+			facilities.name
+	) AS subquery
+
+LIMIT 3;
+
+-- Exercise 20: Classify facilities by value
+SELECT 
+	name,
+	CASE 
+		WHEN class=1 THEN 'high'
+		WHEN class=2 THEN 'average'
+		ELSE 'low'
+		END revenue
+FROM	
+	(
+		SELECT 
+			facilities.name, 
+			NTILE(3) OVER (ORDER BY SUM(CASE 
+						WHEN memid = 0 THEN slots * facilities.guestcost
+						ELSE slots * membercost END) DESC) AS class
+	
+		FROM cd.bookings bookings
+		INNER JOIN cd.facilities facilities
+			ON bookings.facid = facilities.facid
+		GROUP BY facilities.name
+	) AS subquery
+ORDER BY class, name;
+
+-- Exercise 21: Calculate the payback time for each facility
+SELECT 
+	facilities.name,
+	facilities.initialoutlay/((SUM(CASE WHEN memid = 0 THEN slots * guestcost
+										ELSE slots * membercost
+										END) / 3) - facilities.monthlymaintenance) AS months
+	FROM cd.bookings bookings
+	INNER JOIN cd.facilities facilities
+		ON bookings.facid = facilities.facid
+	GROUP BY facilities.facid
+ORDER BY name;
+
+-- Exercise 22: Calculate a rolling average of total revenue
+select 	dategen.date,
+	(
+		select sum(case
+			when memid = 0 then slots * facs.guestcost
+			else slots * membercost
+		end) as rev
+
+		from cd.bookings bks
+		inner join cd.facilities facs
+			on bks.facid = facs.facid
+		where bks.starttime > dategen.date - interval '14 days'
+			and bks.starttime < dategen.date + interval '1 day'
+	)/15 as revenue
+	from
+	(
+		select 	cast(generate_series(timestamp '2012-08-01',
+			'2012-08-31','1 day') as date) as date
+	)  as dategen
+order by dategen.date;   
--- exercises/recursive queries/exercises.sql
+++ exercises/recursive queries/exercises.sql
@@ -1,1 +1,61 @@
 -- Recursive Queries
+
+-- Exercise 1: Find the upward recommendation chain for member ID 27
+WITH RECURSIVE recommenders(recommender) AS (
+	SELECT recommendedby FROM cd.members WHERE memid = 27
+	UNION ALL
+	SELECT mems.recommendedby
+		FROM recommenders recs
+		INNER JOIN cd.members mems
+			ON mems.memid = recs.recommender
+)
+SELECT 
+	recs.recommender,
+	mems.firstname,
+	mems.surname
+FROM 
+	recommenders recs
+INNER JOIN cd.members mems
+	ON recs.recommender = mems.memid
+ORDER BY memid DESC;
+
+-- Exercise 2: Find the downward recommendation chain for member ID 1
+WITH RECURSIVE recommendeds(recommended) AS (
+	SELECT memid FROM cd.members WHERE recommendedby = 1
+	UNION ALL
+	SELECT members.memid 
+		FROM recommendeds recs
+		INNER JOIN cd.members members
+			ON members.recommendedby = recs.recommended
+)
+
+SELECT 
+	recs.recommended AS memid,
+	members.firstname,
+	members.surname
+FROM 
+	recommendeds recs
+INNER JOIN cd.members members	
+	ON recs.recommended = members.memid
+ORDER BY recommended;
+
+-- Exercise 3: Produce a CTE that can return the upward recommendation chain for any member
+WITH RECURSIVE recommenders(recommender, member) AS (
+	SELECT recommendedby, memid FROM cd.members
+	UNION ALL
+	SELECT mems.recommendedby, recs.member
+		FROM recommenders recs
+		INNER JOIN cd.members mems
+			ON mems.memid = recs.recommender
+)
+SELECT 
+	recs.member,
+	recs.recommender,
+	mems.firstname,
+	mems.surname
+FROM 
+	recommenders recs
+INNER JOIN cd.members mems
+	ON recs.recommender = mems.memid
+	WHERE recs.member = 22 OR recs.member = 12
+ORDER BY recs.member ASC, recs.recommender DESC;
